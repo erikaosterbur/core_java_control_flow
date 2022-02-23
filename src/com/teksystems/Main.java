e3edac47442f4002848ffb2f7caba0081ea93997
erikaosterbur <erikaosterbur@gmail.com>
2022-02-23 17:02:59 -0600

    Problem six finished

--- src/com/teksystems/Main.java
+++ src/com/teksystems/Main.java
@@ -10,6 +10,7 @@ public class Main {
         problemThree();
         problemFour();
         problemFive();
+        problemSix();
     }
 
     public static void problemOne () {
@@ -91,6 +92,39 @@ public class Main {
         } else System.out.println("Score out of range");
     }
 
-
+    public static void problemSix () {
+        /*Write a program that accepts an integer between 1 and 7 from the user.
+        Use a switch statement to print out the corresponding weekday.
+        Print “Out of range” if the number is less than 1 or greater than 7.
+        Don’t forget to include “break” statements in each of your cases. */
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter number 1-7: ");
+        int x = input.nextInt();
+        switch(x){
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("Out of range");
+        }
+    }
 
 }
