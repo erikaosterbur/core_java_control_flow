61e9448c7c66ecec4a4580eb15f4388600e5ba66
erikaosterbur <erikaosterbur@gmail.com>
2022-02-23 16:21:11 -0600

    Problem one finished

--- src/com/teksystems/Main.java
+++ src/com/teksystems/Main.java
@@ -3,8 +3,26 @@ package com.teksystems;
 public class Main {
 
     public static void main(String[] args) {
-//        Write a program that declares 1 integer variable x, and then assigns 7 to it
+        problemOne();
+        problemTwo();
+    }
+
+    public static void problemOne () {
+//        Write a program that declares 1 integer variable x, and then assign 7 to it
 //        Write an if statement to print out “Less than 10” if x is less than 10
 //        Change x to equal 15 and notice the result (console should not display anything)
+        int x = 15;
+        if(x<10){
+            System.out.println("Less than 10");
+        }
     }
+
+    public static void problemTwo () {
+//    Write a program that declares 1 integer variable x, and then assigns 7 to it.
+//    Write an if-else statement that prints out “Less than 10” if x is less than 10 and “Greater than 10” if x is greater than 10
+//    Change x to 15 and notice the result.
+
+    }
+
+
 }
