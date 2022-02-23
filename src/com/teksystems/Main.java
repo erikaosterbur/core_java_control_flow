300d6990a59f21a9e222f3abda05d2c393c69c8f
erikaosterbur <erikaosterbur@gmail.com>
2022-02-23 16:56:01 -0600

    Problem four finished

--- src/com/teksystems/Main.java
+++ src/com/teksystems/Main.java
@@ -1,5 +1,7 @@
 package com.teksystems;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -7,6 +9,7 @@ public class Main {
         problemTwo();
         problemThree();
         problemFour();
+        problemFive();
     }
 
     public static void problemOne () {
@@ -60,6 +63,32 @@ public class Main {
         } else{
             System.out.println("In range");
         }
+    }
+
+    public static void problemFive () {
+        /* Write a program that uses if-else-if statements to print out grades A, B, C, D, F according to the following criteria:
+            A: 90-100
+            B: 80-89
+            C: 70-79
+            D: 60-69
+            F: <60
+            Use the Scanner class to accept a number score from the user to determine the letter grade.
+            Print out “Score out of range.” if the score is less than 0 or greater than 100.*/
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter score: ");
+        int x = input.nextInt();
+        if(x>=90 && x<=100){
+            System.out.println("A");
+        } else if(x>=80 && x<=89){
+            System.out.println("B");
+        } else if(x>=70 && x<=79){
+            System.out.println("C");
+        } else if(x>=60 && x<=69){
+            System.out.println("D");
+        } else if(x<60 && x>=0){
+            System.out.println("F");
+        } else System.out.println("Score out of range");
     }
 
 
